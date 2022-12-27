e0f84c5bfda3097a4f272fe8fa3e0440e2ea0c32
rcuello@sismacorporation.com <rcuello.sismasalud@outlook.com>
2022-12-26 23:21:00 -0500

    Delete Product

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -89,6 +89,7 @@ namespace Mango.Services.ProductAPI.Controllers
         }
 
         [HttpDelete]
+        [Route("{id}")]
         public async Task<ResponseDto> Delete(int id)
         {
             try
