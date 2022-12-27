2c863ee50afe69223ccbb021ea2cdab581195c90
rcuello@sismacorporation.com <rcuello.sismasalud@outlook.com>
2022-12-26 22:47:04 -0500

    product list

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -1,7 +1,15 @@
+using Mango.Web;
+using Mango.Web.Services;
+using Mango.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
+
+SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<IProductService, ProductService>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
